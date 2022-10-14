5cae0ae75c0d28cfae005993f0411ea2c9d676cd
IlayMenahem <110230889+IlayMenahem@users.noreply.github.com>
2022-10-14 11:54:58 +0300

    Delete board_test.py

--- board_test.py
+++
@@ -1,9 +0,0 @@
-from abaloneAI.board import board
-import numpy as np
-
-b = board()
-
-key = np.array(((6, 6), (5, 7), (4, 8)),dtype='i1')
-pos = np.array((4, 8),dtype='i1')
-move = np.array(((6, 5), (5, 6), (4, 7)),dtype='i1')
-color = 2
