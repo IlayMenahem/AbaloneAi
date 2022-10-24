f0f8c04f5410e8e86d1ba4373b75801aec730754
Ilay menahem <ilay.menahem@outlook.com>
2022-10-24 19:52:30 +0300

    added ai moudle

---
+++ abaloneAI/AI.py
@@ -0,0 +1,17 @@
+import torch as tr
+from torch import nn
+from torch.nn import functional as func
+from torch.utils.data import DataLoader
+
+import numpy as np
+
+import os
+
+
+#counstactor
+
+#train
+
+#save model
+
+#load model
